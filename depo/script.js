e64f60f0958557c016a4e155a5cfde4c990c68de
NAR <88266823+devnar@users.noreply.github.com>
2022-02-05 13:37:12 +0300

    Update script.js

--- depo/script.js
+++ depo/script.js
@@ -4,8 +4,18 @@ function name() {
         localStorage.setItem("username", person);
     }
     document.getElementById("body").style.background = localStorage.getItem("bgcolor");
-    document.getElementById("datacolor").placeholder = localStorage.getItem("bgcolor");
     document.getElementById("dataname").placeholder = localStorage.getItem("username");
+    if (localStorage.getItem("bgcolor") == null) {
+        document.getElementById("datacolor").placeholder = "#fcc41d";
+    } else {
+        document.getElementById("datacolor").placeholder = localStorage.getItem("bgcolor");
+    }
+}
+
+function cleardata() {
+    localStorage.removeItem("bgcolor");
+    localStorage.removeItem("username");
+    location.reload();
 }
 
 function color() {
@@ -24,41 +34,44 @@ function send() {
     var d = new Date();
     var h = d.getHours(); 
     var m = d.getMinutes();
+    /* HOOKS */
+
     /* kelimeler */
-    var mrb = ["MERHABA","MERHABA LIMON","SELAM","SELAM LIMON","HEY","SLM","MRB"];
-    var sa=["SA","SELAMUNALEYKÜM","SELAMUN ALEYKÜM","SELAMÜN ALEYKÜM","SELAMÜNALEYKÜM"];
-    var sew = ["SENI SEVIYORUM","<3"];
-    var sabah=["GÜNAYDIN","GÜNAYDINLAR","GÜNAYDIN LIMON"];
-    var kotu=["KÖTÜ","BERBAT","YARARSIZ","ÇALIŞMIYOR","IYI DEĞIL",":(",":c"];
-    var ia=["IYI AKŞAM","IYI AKŞAMLAR","IYI AKŞAMLAR LIMON"];
-    var ig=["IYI GECELER","UYKULUYUM","IYI UYKULAR","IYI GECELER LIMON"];
-    var guzel=["GÜZEL ÇALIŞIYOR","GÜZEL KOD","GÜZEL","VAY","VAYY","VAYYY","ÇALIŞIYOR","HARIKA","GÜZEL","MUHTEŞEM","IYI","AMAN ALLAHIM","UWU","SENINLE TANIŞMAK GüZEL","SENI GÖRMEK GÜZEL","HARIKASIN"];
-    var tun=["TÜNAYDIN","TÜNAYDINLAR","TÜNAYDIN LIMON"];
-    var nasil=["NASILSIN","IYI MISIN", "NASILSIN?", "IYI MISIN?","SEN NASILSIN","SEN NASILSIN?","NABER","NABER?"];
-    var nap=["NAPIYON?","NE YAPIYORSUN","NE YAPIYON", "NAPIYORSUN","NE YAPIYORSUN?"];
-    var iyi=["IYI","👍🏻", ":)","c:","😀","😁","😃","😄","😆","😊","🙂","😉","BEN DE","EVET","IŞTE BU","IYI SEN?","IYI SEN","IYIYIM"];
+    var mrb = ["merhaba","merhaba limon","selam","selam limon","slm","mrb"];
+    var sa=["sa","selamunaleyküm","selamun aleyküm","selamün aleyküm","selamünaleyküm"];
+    var sew = ["seni seviyorum","<3"];
+    var sabah=["günaydın","günaydınlar","günaydın limon"];
+    var kotu=["kötü","berbat","yararsız","çalışmıyor","iyi değil",":(",":c"];
+    var ia=["iyi akşam","iyi akşamlar","iyi akşamlar limon"];
+    var ig=["iyi geceler","uykuluyum","iyi uykular","iyi geceler limon"];
+    var guzel=["güzel çalışıyor","güzel kod","güzel","vay","vayy","vayyy","çalışıyor","mükemmel","harika","güzel","muhteşem","iyi","aman allahım","uwu","seninle tanışmak güzel","seni görmek güzel","harikasin"];
+    var tun=["tünaydın","tünaydınlar","tünaydın limon"];
+    var nasil=["nasılsın","iyi misin", "nasılsın?", "iyi misin?","sen nasılsın","sen nasılsın?","naber","naber?"];
+    var nap=["napıyon?","ne yapıyorsun","ne yapıyon", "napıyorsun","ne yapıyorsun?"];
+    var iyi=["iyi","👍🏻", ":)","c:","😀","😁","😃","😄","😆","😊","🙂","😉","ben de","evet","işte bu","iyi sen?","iyi sen","iyiyim"];
+    var ses=["limon","knk","hey","sen","ordaki","oradaki"]
 
-
-    if (msg.value == "") {
+    if (msg.value == " ") {
         alert("bir mesaj girmelisin")
     } else {
         box.innerHTML += "<div class='message sent'>" + msg.value + "<span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>";
         
         /* cevaplar */
         function isInArray(x, y) {return x.indexOf(y) > -1;}
-        isInArray(mrb, msg.value.toUpperCase())==true?(box.innerHTML += "<div class='message received'> Merhaba " + localStorage.getItem("username") + " :) nasılsın?<span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>", msg.value = "", msg.focus()):
-        isInArray(sa, msg.value.toUpperCase())==true?(box.innerHTML += "<div class='message received'> Aleykümselam " + localStorage.getItem("username") + " :) nasılsın?<span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>", msg.value = "", msg.focus()):
-        isInArray(sabah, msg.value.toUpperCase())==true?(box.innerHTML += "<div class='message received'> Sanada günaydın " + localStorage.getItem("username") + " :) nasılsın?<span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>", msg.value = "", msg.focus()):
-        isInArray(tun, msg.value.toUpperCase())==true?(box.innerHTML += "<div class='message received'> Sanada tünaydın " + localStorage.getItem("username") + " :) nasılsın?<span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>", msg.value = "", msg.focus()):
-        isInArray(ia, msg.value.toUpperCase())==true?(box.innerHTML += "<div class='message received'> Sanada iyi akşamlar " + localStorage.getItem("username") + " görüşmek üzere <span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>", msg.value = "", msg.focus()):
-        isInArray(ig, msg.value.toUpperCase())==true?(box.innerHTML += "<div class='message received'> Sanada iyi geceler " + localStorage.getItem("username") + " görüşmek üzere 🥱 <span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>", msg.value = "", msg.focus()):
-        isInArray(sew, msg.value.toUpperCase())==true?(box.innerHTML += "<div class='message received'> Bende seni seviyorum " + localStorage.getItem("username") + " 🥰 <span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>", msg.value = "", msg.focus()):
-        isInArray(kotu, msg.value.toUpperCase())==true?(box.innerHTML += "<div class='message received'> Böyle düşünmen beni üzdü 😭 <span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>", msg.value = "", msg.focus()):
-        isInArray(iyi, msg.value.toUpperCase())==true?(box.innerHTML += "<div class='message received'> Bunu duymak harika 😃 <span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>", msg.value = "", msg.focus()):
-        isInArray(guzel, msg.value.toUpperCase())==true?(box.innerHTML += "<div class='message received'> Böyle düşünmen beni sevindirdi 😊 <span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>", msg.value = "", msg.focus()):
-        isInArray(nasil, msg.value.toUpperCase())==true?(box.innerHTML += "<div class='message received'> İyiyim Peki sen? <span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>", msg.value = "", msg.focus()):
-        isInArray(nap, msg.value.toUpperCase())==true?(box.innerHTML += "<div class='message received'> Senle konuşuyorum 😉 <span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>", msg.value = "", msg.focus()):
-        (box.innerHTML += "<div class='message received'>üzgünüm ama anlamadım<br><b align='center'>Bana bu kelimeyi öğretir misin?</b><br><br><input type='text' id='soru' placeholder='sorulacak soru' class='input'><br><input type='text' id='cevap' placeholder='cevabı' class='input'><br><button onclick='formSend1()' class='button'>Gönder</button><span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>", msg.value = "", msg.focus());
+        isInArray(mrb, msg.value.toLowerCase())==true?(box.innerHTML += "<div class='message received'> Merhaba " + localStorage.getItem("username") + " :) nasılsın?<span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>", msg.value = "", msg.focus()):
+        isInArray(sa, msg.value.toLowerCase())==true?(box.innerHTML += "<div class='message received'> Aleykümselam " + localStorage.getItem("username") + " :) nasılsın?<span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>", msg.value = "", msg.focus()):
+        isInArray(sabah, msg.value.toLowerCase())==true?(box.innerHTML += "<div class='message received'> Sanada günaydın " + localStorage.getItem("username") + " :) nasılsın?<span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>", msg.value = "", msg.focus()):
+        isInArray(tun, msg.value.toLowerCase())==true?(box.innerHTML += "<div class='message received'> Sanada tünaydın " + localStorage.getItem("username") + " :) nasılsın?<span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>", msg.value = "", msg.focus()):
+        isInArray(ia, msg.value.toLowerCase())==true?(box.innerHTML += "<div class='message received'> Sanada iyi akşamlar " + localStorage.getItem("username") + " görüşmek üzere <span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>", msg.value = "", msg.focus()):
+        isInArray(ig, msg.value.toLowerCase())==true?(box.innerHTML += "<div class='message received'> Sanada iyi geceler " + localStorage.getItem("username") + " görüşmek üzere 🥱 <span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>", msg.value = "", msg.focus()):
+        isInArray(sew, msg.value.toLowerCase())==true?(box.innerHTML += "<div class='message received'> Bende seni seviyorum " + localStorage.getItem("username") + " 🥰 <span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>", msg.value = "", msg.focus()):
+        isInArray(kotu, msg.value.toLowerCase())==true?(box.innerHTML += "<div class='message received'> Böyle düşünmen beni üzdü 😭 <span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>", msg.value = "", msg.focus()):
+        isInArray(iyi, msg.value.toLowerCase())==true?(box.innerHTML += "<div class='message received'> Bunu duymak harika 😃 <span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>", msg.value = "", msg.focus()):
+        isInArray(guzel, msg.value.toLowerCase())==true?(box.innerHTML += "<div class='message received'> Böyle düşünmen beni sevindirdi 😊 <span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>", msg.value = "", msg.focus()):
+        isInArray(ses, msg.value.toLowerCase())==true?(box.innerHTML += "<div class='message received'> Bana seslendin sanırım. Buyur? <span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>", msg.value = "", msg.focus()):
+        isInArray(nasil, msg.value.toLowerCase())==true?(box.innerHTML += "<div class='message received'> İyiyim Peki sen? <span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>", msg.value = "", msg.focus()):
+        isInArray(nap, msg.value.toLowerCase())==true?(box.innerHTML += "<div class='message received'> Senle konuşuyorum 😉 <span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>", msg.value = "", msg.focus()):
+        (box.innerHTML += "<div class='message received'>üzgünüm ama anlamadım<br><b align='center'>Bana bu kelimeyi öğretir misin?</b><br><br><input type='text' id='soru' placeholder='sorulacak soru' class='input'><br><input type='text' id='cevap' placeholder='cevabı' class='input'><br><button onclick='formSend()' class='button'>Gönder</button><span class='metadata'> <span class='time'>" + h +":"+ m +"</span></span></div>", msg.value = "", msg.focus());
     }
 }
 
@@ -80,5 +93,5 @@ function runSpeechRecognition() {
         document.getElementById("val").value = transcript;
         console.log(confidence*100+"%")
     };
-     recognition.start();
+    recognition.start();
 }
